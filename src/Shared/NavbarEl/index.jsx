00cce68697ac79d5bbe880977e3781ce4e1386e7
adityawintech <aditya#19AI>
2024-11-19 19:49:14 +0530

    Aditya Bottom Navigation

--- src/Shared/NavbarEl/index.jsx
+++ src/Shared/NavbarEl/index.jsx
@@ -47,7 +47,7 @@ const NavbarEl = () => {
 
   return (
     <>
-      <nav className={`navbar ${isSticky ? 'sticky-navbar' : ''}`}>
+      <nav className={`navbar ${isSticky ? 'sticky-navbar' : ''}`} style={{top:"-3px"}}>
         {/* Logo */}
         <div className="navbar-brand">
           <Link to="/">
