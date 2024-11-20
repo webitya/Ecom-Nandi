e2b2a8fa0f6db15e6732a5051618c97562801cdd
BEaANIKET <aniketchaturvedi309@gmail.com>
2024-11-20 15:59:01 +0530

    asdf

--- src/Pages/Verify/index.jsx
+++ src/Pages/Verify/index.jsx
@@ -11,7 +11,7 @@ const Verify = () => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-          const fetching= async() => {
+        const fetching = async () => {
             try {
                 const response = await useRequestApi(`api/auth/verify?token=${token}`, 'POST');
                 console.log('response ', response)
@@ -22,7 +22,7 @@ const Verify = () => {
             } catch (error) {
                 console.log("error ", error)
                 setError(error?.response?.data?.message || error.message || 'some error')
-                toast.error(error?.response?.data?.message  || error.message || 'some error')
+                toast.error(error?.response?.data?.message || error.message || 'some error')
                 setTimeout(() => {
                     navigation('/register')
                 }, 2000)
@@ -49,11 +49,11 @@ const Verify = () => {
                         <a href="/" className="mt-4 text-blue-500">Go back to Home</a>
                     </div>
                     :
-                    <div className="text-center">
-                        <h1 className="text-6xl font-bold text-red-500">404</h1>
-                        <p className="text-2xl">Page Not Found</p>
-                        <a href="/" className="mt-4 text-blue-500">Go back to Home</a>
-                    </div>
+            <div className="text-center">
+                <h1 className="text-6xl font-bold text-red-500">404</h1>
+                <p className="text-2xl">Page Not Found</p>
+                <a href="/" className="mt-4 text-blue-500">Go back to Home</a>
+            </div>
             }
         </div>
     );
